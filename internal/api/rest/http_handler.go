b698449519b26a000d79b95b01b6c5861be62ae4
mehmet-dogru <mdogru685@gmail.com>
2024-03-12 11:41:51 +0300

    feat: implemented auth service

--- internal/api/rest/http_handler.go
+++ internal/api/rest/http_handler.go
@@ -2,10 +2,12 @@ package rest
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"go-blog-app/internal/helper"
 	"gorm.io/gorm"
 )
 
 type RestHandler struct {
-	App *fiber.App
-	DB  *gorm.DB
+	App  *fiber.App
+	DB   *gorm.DB
+	Auth helper.Auth
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -5,6 +5,8 @@ import (
 	"go-blog-app/config"
 	"go-blog-app/internal/api/rest"
 	"go-blog-app/internal/api/rest/handlers"
+	"go-blog-app/internal/domain"
+	"go-blog-app/internal/helper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -20,9 +22,14 @@ func StartServer(config config.AppConfig) {
 		log.Println("database connection success ✅")
 	}
 
+	db.AutoMigrate(&domain.User{})
+
+	auth := helper.SetupAuth(config.AppSecret)
+
 	rh := &rest.RestHandler{
-		App: app,
-		DB:  db,
+		App:  app,
+		DB:   db,
+		Auth: auth,
 	}
 
 	setupRoutes(rh)
