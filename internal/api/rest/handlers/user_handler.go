f56a125a4778cb9a8405c2573cf24837d7ec0684
mehmet-dogru <mdogru685@gmail.com>
2024-03-12 11:42:43 +0300

    feat: added register and login operations

--- internal/api/rest/handlers/user_handler.go
+++ internal/api/rest/handlers/user_handler.go
@@ -4,16 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go-blog-app/internal/api/rest"
 	"go-blog-app/internal/api/rest/responses"
+	"go-blog-app/internal/dto"
+	"go-blog-app/internal/repository"
+	"go-blog-app/internal/service"
 	"net/http"
 )
 
 type UserHandler struct {
+	svc service.UserService
 }
 
 func SetupUserRoutes(rh *rest.RestHandler) {
 	app := rh.App
 
-	handler := UserHandler{}
+	repo := repository.NewUserRepository(rh.DB)
+	svc := service.NewUserService(repo, rh.Auth)
+
+	handler := UserHandler{
+		svc: svc,
+	}
 
 	pubRoutes := app.Group("/users")
 
@@ -24,10 +33,33 @@ func SetupUserRoutes(rh *rest.RestHandler) {
 }
 
 func (h *UserHandler) Register(ctx *fiber.Ctx) error {
-	return responses.NewSuccessResponse(ctx, http.StatusOK, "Register")
+	user := dto.UserSignup{}
+
+	err := ctx.BodyParser(&user)
+	if err != nil {
+		return responses.NewErrorResponse(ctx, http.StatusBadRequest, "please provide valid inputs")
+	}
+
+	token, err := h.svc.Signup(user)
+	if err != nil {
+		return responses.NewErrorResponse(ctx, http.StatusBadRequest, "error on signup")
+	}
+
+	return responses.NewSuccessResponse(ctx, http.StatusOK, token)
 }
 
 func (h *UserHandler) Login(ctx *fiber.Ctx) error {
-	return responses.NewSuccessResponse(ctx, http.StatusOK, "Login")
+	loginInput := dto.UserLogin{}
 
+	err := ctx.BodyParser(&loginInput)
+	if err != nil {
+		return responses.NewErrorResponse(ctx, http.StatusBadRequest, "please provide valid inputs")
+	}
+
+	token, err := h.svc.Login(loginInput.Email, loginInput.Password)
+	if err != nil {
+		return responses.NewErrorResponse(ctx, http.StatusUnauthorized, err.Error())
+	}
+
+	return responses.NewSuccessResponse(ctx, http.StatusOK, token)
 }
