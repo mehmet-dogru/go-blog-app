66e040c9d3a973527b1387aed903eb7d4197c784
mehmet-dogru <mdogru685@gmail.com>
2024-03-18 10:53:59 +0300

    feat: initialized validator

---
+++ pkg/utils/validator/validator.go
@@ -0,0 +1,16 @@
+package validator
+
+import (
+	"context"
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+func ValidateStruct(ctx context.Context, s interface{}) error {
+	return validate.StructCtx(ctx, s)
+}
